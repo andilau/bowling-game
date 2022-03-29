4ad64fa5ce2e4780998f9c8a98be41cef5e499c0
Andi Lau <andi@herrlau.de>
2022-03-29 08:26:01 +0200

    Fix Gradle run task
    - Per default the system.in of your gradle build is not wired up with the system.in of the run (JavaExec) task.
    
    - https://stackoverflow.com/questions/13172137/console-application-with-java-and-gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ group = "de.herrlau"
 version = "1.0-SNAPSHOT"
 
 application {
-    mainClass.set("MainKt")
+    mainClass.set("de.herrlau.bowlinggame.BowlingGameApp")
 }
 
 repositories {
@@ -21,8 +21,46 @@ dependencies {
     testImplementation("org.assertj:assertj-core:3.22.0")
 }
 
+// Per default the system.in of your gradle build is not wired up with the system.in of the run (JavaExec) task.
+
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in`
+}
+
+tasks {
+    val fatJar = register<Jar>("fatJar") {
+        dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources")) // We need this for Gradle optimization to work
+        archiveClassifier.set("standalone") // Naming the jar
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest { attributes(mapOf("Main-Class" to application.mainClass)) } // Provided we set it up in the application plugin configuration
+        val sourcesMain = sourceSets.main.get()
+        val contents = configurations.runtimeClasspath.get()
+            .map { if (it.isDirectory) it else zipTree(it) } +
+                sourcesMain.output
+        from(contents)
+    }
+    build {
+        dependsOn(fatJar) // Trigger fat jar creation during build
+    }
+}
+
 tasks.test {
     useJUnitPlatform()
+
+    testLogging {
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        showCauses = true
+        showExceptions = true
+        showStackTraces = true
+        showStandardStreams = true
+        events = setOf(
+            org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED,
+            org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED,
+            org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
+            org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_OUT,
+            org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_ERROR
+        )
+    }
 }
 
 tasks.withType<KotlinCompile> {
